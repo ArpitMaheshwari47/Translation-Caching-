273653b4f723a4650f03d41aa3eb71a8997298ca
arpitmaheshwar <apmaheshwari47@gmail.com>
2022-12-13 17:41:55 +0530

    finallycode

--- routes/translate.js
+++ routes/translate.js
@@ -4,11 +4,11 @@ const router = express.Router();
 const googleTranslate = require('@vitalets/google-translate-api');
 
 router.get('/translate', cache(10), async (req, res) => {
-    let queryParamter = req.query;
+    let queryParameter = req.query;
 
     let output = {};
     try {
-        const response = await googleTranslate(queryParamter.textFormat, { to: queryParamter.targetLanguage });
+        const response = await googleTranslate(queryParameter.sourceText, { to: queryParameter.targetLanguage });
         output.translatedText = response.text;
         output.fromLanguage = response.from.language.iso;
     } catch (error) {
@@ -38,11 +38,11 @@ router.get('/test1', async (req,res) => {
 });
 
 router.get('/test2', async (req, res) => {
-    let queryParamter = req.query;
+    let queryParameter = req.query;
 
     let output = {};
     try {
-        const response = await googleTranslate(queryParamter.textFormat, { to: queryParamter.targetLanguage });
+        const response = await googleTranslate(queryParameter.sourceText, { to: queryParameter.targetLanguage });
 
         output.translatedText = response.text;
         output.fromLanguage = response.from.language.iso;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
